7637cd86c4bdb630daebda6ac4f592a825099681
Solo <juanoliugomezplatero@gmail.com>
2026-01-19 01:22:37 -0300

    base graph

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from langraph-therapist-ai-agent!")
+from typing import Annotated, TypedDict
+
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from langgraph.graph import END, START, StateGraph
+from langgraph.graph.message import add_messages
+
+load_dotenv()
+
+llm = init_chat_model(model="anthropic:claude-sonnet-3-5-latest")
 
 
-if __name__ == "__main__":
-    main()
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+
+graph_builder = StateGraph(State)
+
+
+def chatbot(state: State):
+    return {"messages": [llm.invoke(state["messages"])]}
